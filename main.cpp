9a153dd7175abd5fde324463ff0c7f23f98b9deb
Michael Bartling <michael.bartling@arm.com>
2018-07-27 12:28:52 -0500

    Testing softmax

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 #include "FATFileSystem.h"
 #include "F413ZH_SD_BlockDevice.h"
 #include "FullyConnectedOps.hpp"
+#include "softmax.hpp"
 
 
 F413ZH_SD_BlockDevice bd;
 FATFileSystem fs("fs");
 
-Context ctx;
+
 
 template <typename T1, typename T2, typename T3>
 void test_FC(int scratch_space, T1 x, T2 y){
+    Context ctx;
     ctx.gc();
 
     S_TENSOR A = ctx.add(new RamTensor<T1>({2,2}), "A");
@@ -58,12 +60,8 @@ void test_FC(int scratch_space, T1 x, T2 y){
 
 }
 
-
-int main(){
-    printf("Starting Test\n\r");
-    ON_ERR(bd.init(), "SDBlockDevice init ");
-    ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
-
+void test_mat_mul(){
+    printf("Testing FC\n\r");
     printf("Testing q7_t\n\r");
     test_FC<q7_t, q7_t, q7_t>(2, 0, 0);
     
@@ -72,5 +70,55 @@ int main(){
     
     printf("Testing q15_t q7_t hybrid\n\r");
     test_FC<q15_t, q7_t, q7_t>(2, 0, 0);
+}
+
+template <typename T1>
+void do_softmax(T1 x){
+    Context ctx;
+    ctx.gc();
+
+    S_TENSOR A = ctx.add(new RamTensor<T1>({4,1}), "A");
+    S_TENSOR out = ctx.add(new RamTensor<T1>({4,1}), "out");
+
+   // A.resize({2,2});
+    //T1* a = A->write<T1>(0, sizeof(T1));
+    T1* a = A->write<T1>(0, 0);
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    
+    TNameList inputs = {"A"};
+    TNameList outputs = {"out"};
+  
+    ctx.push_static(hold(new SoftmaxCmsisOp<T1>()), "SoftmaxOp", inputs, outputs);
+    ctx.eval();
+    
+    //S_TENSOR out_s = ctx.get({"out"});
+    const T1* out_1 = out->read<T1>(0, 0);
+    // Should be [24]
+    //           [47]
+    printf("[%d]\n\r", out_1[0]);
+    printf("[%d]\n\r", out_1[1]);
+    printf("[%d]\n\r", out_1[2]);
+    printf("[%d]\n\r", out_1[3]);
+
+}
+void test_softmax(){
+    printf("Testing softmax\n\r");
+    printf("Testing q7\n\r");
+    do_softmax<q7_t>(0);
+    printf("Testing q15\n\r");
+    do_softmax<q15_t>(0);
+}
+
+int main(){
+    printf("Starting Test\n\r");
+    ON_ERR(bd.init(), "SDBlockDevice init ");
+    ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
+
+    test_mat_mul();
+    test_softmax();
+
 
 }
